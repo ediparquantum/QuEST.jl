3b8a441eaf6112f6295ecc5556e37ceb01dfba71
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-02-19 12:40:58 +0000

    removed travis.yml

---
+++ tests_to_be_deleted.jl
@@ -0,0 +1,13 @@
+##################################################################
+# Filename  : tests_to_be_deleted.jl
+# Author    : Jonathan Miller
+# Date      : 2024-02-19
+# Aim       : aim_script
+#           : Add trace functions to the code
+#           :
+##################################################################
+
+using Pkg
+Pkg.activate(".")
+using QuEST
+
