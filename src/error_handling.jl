63e9cfbb3a6ccfffbf1d22212714de1e2ddba876
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-06-05 14:22:33 +0100

    Removed Int64 requirement for test_qubit_present method

--- src/error_handling.jl
+++ src/error_handling.jl
@@ -9,7 +9,7 @@ struct QubitsNotInQuregError <: AbstractJulia2CSyntexError end
 function throw_error(::QubitsNotInQuregError)
     error("Qubits not in qureg, Segmentation fault will occur. Throwing error to prevent this.")
 end
-function test_qubit_present(qureg::Qureg,qubits::Union{Int,Array{Int}})
+function test_qubit_present(qureg::Qureg,qubits)
     
     if !(all(qubits .∈ Ref(Base.OneTo(qureg.numQubitsRepresented))))
         throw_error(QubitsNotInQuregError())
